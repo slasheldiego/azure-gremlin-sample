75b96d450d7931952d3e5ed35d6cace0656d0c76
Diego Benavides <dbenavidesv@me.com>
2020-12-23 00:59:36 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {};
+
+config.endpoint = "wss://frabricagrafos.gremlin.cosmos.azure.com:443";
+config.authKey =
+  "FHfT6QPnQRCbSlH61Gz1fL6ERLH1jKetPaswNw3hxivMmQFDsBxuxmZLLQPAdujsmeMnMNGWDbdOSUnBh8H6Lw==";
+config.database = "sample-database";
+config.collection = "sample-graph";
+
+module.exports = config;
